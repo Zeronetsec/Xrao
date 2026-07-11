ac41972fd637cbab86d0fff1169335981a582285
Zeronetsec <senxhunter996@gmail.com>
2026-07-11 15:13:14 +0700

    modified: console/uwu.go

--- console/uwu.go
+++ console/uwu.go
@@ -13,6 +13,8 @@ func (c Uwu) Execute(args []string) {
     fmt.Printf("\x1b[?25l")
     uwu.Nyaa(5 * time.Second)
     fmt.Printf("\x1b[?25h")
+
+    fmt.Println()
 }
 
 // Copyright (c) 2026 Zeronetsec
